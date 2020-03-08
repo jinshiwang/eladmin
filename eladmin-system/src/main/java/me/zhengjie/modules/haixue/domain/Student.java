9f3192fac33cb082e97958afa02fcef351769cd1
王金石 <jinshi.wang@ly.com>
2020-03-08 22:50:33 +0800

    添加学生报表功能-添加表格

--- eladmin-system/src/main/java/me/zhengjie/modules/haixue/domain/Student.java
+++ eladmin-system/src/main/java/me/zhengjie/modules/haixue/domain/Student.java
@@ -85,10 +85,6 @@ public class Student {
     @Column(name = "gmt_time")
     @CreationTimestamp
     private Timestamp gmtTime;
-    /**
-     * 学生总数
-     */
-    private Integer total;
 
     public void copy(Student source){
         BeanUtil.copyProperties(source,this, CopyOptions.create().setIgnoreNullValue(true));
